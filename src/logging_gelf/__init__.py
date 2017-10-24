1021115ddb63c01c12ec069cc0187a5f00af5ab8
Cédric Dumay <cedric.dumay@gmail.com>
2017-10-24 12:11:19 +0200

    fix: extra on LoggerAdapter logger methods are not used

--- src/logging_gelf/__init__.py
+++ src/logging_gelf/__init__.py
@@ -12,3 +12,25 @@ SYSLOG_LEVELS = {
     logging.CRITICAL: 2, logging.ERROR: 3, logging.WARNING: 4, logging.INFO: 6,
     logging.DEBUG: 7,
 }
+
+
+## monkey patch
+def process(self, msg, kwargs):
+    """
+    Process the logging message and keyword arguments passed in to
+    a logging call to insert contextual information. You can either
+    manipulate the message itself, the keyword args or both. Return
+    the message and kwargs modified (or not) to suit your needs.
+
+    Normally, you'll only need to override this one method in a
+    LoggerAdapter subclass for your specific needs.
+    """
+    extra = dict(self.extra)
+    extra.update(kwargs.get('extra', dict()))
+    kwargs["extra"] = extra
+    return msg, kwargs
+
+
+from logging import LoggerAdapter
+
+LoggerAdapter.process = process
